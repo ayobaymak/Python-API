9f8a1dedb938ce1acdd058be25078efe5fc79665
Segun Salami <Segun.Salami@appliedis.com>
2022-05-10 21:58:36 +0100

    Added put, delete and get specific student by ID  methods

--- api.py
+++ api.py
@@ -33,7 +33,36 @@ class StudentsList(Resource):
     }
     return STUDENTS[student_id], 201
 
+class Student(Resource):
+  def get(self, student_id):
+    if student_id not in STUDENTS:
+      return "Not Found", 404
+    else:
+      return STUDENTS[student_id]
+
+  def put(self, student_id):
+    parser.add_argument("name", location='args')
+    parser.add_argument("age", type=int, location='args')
+    parser.add_argument("spec", location='args')
+    args = parser.parse_args()
+    if student_id not in STUDENTS:
+      return "Record not Found", 404
+    else:
+      student = STUDENTS[student_id]
+      student["name"] = args["name"] if args["name"] is not None else student["name"]
+      student["age"] = args["age"] if args["age"] is not None else student["age"]
+      student["spec"] = args["spec"] if args["spec"] is not None else student["spec"]
+      return student, 200
+      
+  def delete(self, student_id):
+    if student_id not in STUDENTS:
+      return "Record not Found", 404
+    else:
+      del STUDENTS[student_id]
+      return '', 204
+
 api.add_resource(StudentsList, '/students/')
+api.add_resource(Student, '/students/<student_id>')
 
 if __name__ == "__main__":
   app.run(debug=True)
